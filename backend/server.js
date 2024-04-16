99182bdec18574c98001fb4f819c719c76c85e1f
Jherson Chavez <jhersondannychavezmontes@gmail.com>
2024-04-15 23:38:51 -0500

    colocado usersidebar

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./database/connectToMongoDB.js";
 import messageRoutes from "./models/message.model.js";
+import userRoutes from "./routes/user.routes.js";
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -16,6 +18,8 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
 
 
 //app.get("/", (req, res) =>{
