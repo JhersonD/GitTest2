4858e0fe87dbebc74a38a72585ca7bde141bd147
Jherson Chavez <jhersondannychavezmontes@gmail.com>
2024-04-17 01:22:40 -0500

    1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
-
+import path from "path"
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./database/connectToMongoDB.js";
 import messageRoutes from "./models/message.model.js";
@@ -10,6 +10,8 @@ import userRoutes from "./routes/user.routes.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,13 +22,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
-//app.get("/", (req, res) =>{
-    // root route http://localhost:5000/
-//    res.send("Hello world!!")
-//});
-
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT,() => {
     connectToMongoDB()
